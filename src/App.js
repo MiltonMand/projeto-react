dad0a069651370c64ab6dedf6a79a369b3a77f50
Milton Mand <johnstak82@gmail.com>
2024-09-07 23:52:15 +0200

    organização de arquivos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Company from './components/Company';
+import Contact from './components/Contact';
+import MyProject from './components/MyProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/company'>Company</Link>
+        </li>
+        <li>
+          <Link to='/contact'>Contact</Link>
+        </li>
+        <li>
+          <Link to='/myproject'>MyProjects</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/company' element={<Company />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/myproject' element={<MyProject />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/MyProject.js
@@ -0,0 +1,9 @@
+function MyProject() {
+    return (
+        <div>
+            <h1>MyProjects</h1>
+        </div>
+    )
+}
+
+export default MyProject
